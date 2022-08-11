de92a099437ff9e267a59af1cd3b9c860a9c1997
Jason Laster <jason.laster.11@gmail.com>
2022-08-10 17:14:18 -0700

    Test PR

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,13 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("homepage has Playwright in title and get started link linking to the intro page", async ({
+  page,
+}) => {
+  await page.goto("https://playwright.dev/");
 
   // // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Puppet/);
 
   // // create a locator
   // const getStarted = page.locator('text=Get Started');
@@ -17,5 +19,5 @@ test('homepage has Playwright in title and get started link linking to the intro
   // await getStarted.click();
 
   // Expects the URL to contain intro.
-  await expect(true).toEqual(false);
+  // await expect(true).toEqual(false);
 });
