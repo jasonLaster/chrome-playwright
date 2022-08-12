7a499d339f915a181991335ef296b85f95dc210f
Jason Laster <jason.laster.11@gmail.com>
2022-08-12 10:32:29 -0700

    Drop overtone preview branch

--- tests/overtone.spec.ts
+++ tests/overtone.spec.ts
@@ -4,26 +4,26 @@ import { test, expect } from "@playwright/test";
 test("homepage has Playwright in title and get started link linking to the intro page", async ({
   page,
 }) => {
-  // Go to https://62f2b4ee90fc0f08f669230f--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:5gDdqXX8gWNvT2aPmwjB5V
+  // Go to https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:5gDdqXX8gWNvT2aPmwjB5V
   await page.goto(
-    "https://62f2b4ee90fc0f08f669230f--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:5gDdqXX8gWNvT2aPmwjB5V"
+    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:5gDdqXX8gWNvT2aPmwjB5V"
   );
 
   // Click text=😌 Mellow >> nth=1
   await page.locator("text=😌 Mellow").nth(1).click();
   await expect(page).toHaveURL(
-    "https://62f2b4ee90fc0f08f669230f--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:6jDxA8bflHbeRHNSDxaX2L"
+    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:6jDxA8bflHbeRHNSDxaX2L"
   );
 
   // Click text=V Recordings >> nth=1
   await page.locator("text=V Recordings").nth(1).click();
   await expect(page).toHaveURL(
-    "https://62f2b4ee90fc0f08f669230f--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:0xNTCYUe9X8A8ITnmq6hMt"
+    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:0xNTCYUe9X8A8ITnmq6hMt"
   );
 
   // Click text=Tech House >> nth=1
   await page.locator("text=Tech House").nth(1).click();
   await expect(page).toHaveURL(
-    "https://62f2b4ee90fc0f08f669230f--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:618pCYz9YhautVBYwyjFVM"
+    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:618pCYz9YhautVBYwyjFVM"
   );
 });
