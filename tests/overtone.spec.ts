a7666197af63c8ad8e6263694b930c194ae92d2a
Jason Laster <jason.laster.11@gmail.com>
2022-09-28 08:50:30 -0700

    update overtone (#7)

--- tests/overtone.spec.ts
+++ tests/overtone.spec.ts
@@ -6,24 +6,24 @@ test("homepage has Playwright in title and get started link linking to the intro
 }) => {
   // Go to https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:5gDdqXX8gWNvT2aPmwjB5V
   await page.goto(
-    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:5gDdqXX8gWNvT2aPmwjB5V"
+    "https://633361d56c2366088366fb35--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:5gDdqXX8gWNvT2aPmwjB5V"
   );
 
   // Click text=😌 Mellow >> nth=1
   await page.locator("text=😌 Mellow").nth(1).click();
   await expect(page).toHaveURL(
-    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:6jDxA8bflHbeRHNSDxaX2L"
+    "https://633361d56c2366088366fb35--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:6jDxA8bflHbeRHNSDxaX2L"
   );
 
   // Click text=V Recordings >> nth=1
   await page.locator("text=V Recordings").nth(1).click();
   await expect(page).toHaveURL(
-    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:0xNTCYUe9X8A8ITnmq6hMt"
+    "https://633361d56c2366088366fb35--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:0xNTCYUe9X8A8ITnmq6hMt"
   );
 
   // Click text=Tech House >> nth=1
   await page.locator("text=Tech House").nth(1).click();
   await expect(page).toHaveURL(
-    "https://overtone-demo.netlify.app/spotify/playlist/spotify:playlist:618pCYz9YhautVBYwyjFVM"
+    "https://633361d56c2366088366fb35--overtone-demo.netlify.app/spotify/playlist/spotify:playlist:618pCYz9YhautVBYwyjFVM"
   );
 });
